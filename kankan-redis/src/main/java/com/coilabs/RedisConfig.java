ef940ff2c10ed5b7e6a2312609fa80c7ce2f0c3a
runoob <test@runoob.com>
2018-03-29 17:32:06 +0800

    spring boot redis 测试

---
+++ kankan-redis/src/main/java/com/coilabs/RedisConfig.java
@@ -0,0 +1,56 @@
+
+package com.coilabs;
+
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+
+@Configuration
+@EnableCaching//XML文件中配置cache manager
+public class RedisConfig  extends CachingConfigurerSupport {
+
+
+/**
+     *生成id
+     * @return
+     */
+
+/*
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+
+        return new KeyGenerator() {
+
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }*/
+
+}
+
---
+++ kankan-redis/src/main/java/com/coilabs/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.coilabs.controller;
+
+
+import com.coilabs.modle.User;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/get")
+    @Cacheable(value="user-key",key = "#key+#name")
+    public List<User> getRedis(String key, String name) {
+        List<User> list=new ArrayList<>();
+        User modle=new User();
+        modle.setId("get");
+        modle.setName("get");
+        list.add(modle);
+        list.add(modle);
+        System.out.print("meizhouredis");
+        return  list;
+    }
+    @RequestMapping("/set")
+    @CachePut(value="user-key",key = "#key+#name")
+    public List<User> setRedis(String key ,String name) {
+        List<User> list=new ArrayList<>();
+        User modle=new User();
+        modle.setId("set");
+      //  modle.setName("get");
+        list.add(modle);
+        list.add(modle);
+        System.out.print("meizhouredis");
+        return  list;
+    }
+}
