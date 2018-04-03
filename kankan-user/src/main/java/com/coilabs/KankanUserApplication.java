37393b1bf69b7b88361437d496a8f4cf616c1974
runoob <test@runoob.com>
2018-04-03 17:12:38 +0800

    修改提交 暂时解决报错问题

--- kankan-user/src/main/java/com/coilabs/KankanUserApplication.java
+++ kankan-user/src/main/java/com/coilabs/KankanUserApplication.java
@@ -1,9 +1,10 @@
 package com.coilabs;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class KankanUserApplication {
 
 	public static void main(String[] args) {
